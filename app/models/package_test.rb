dba2c0c07864a8f69de15ab9c34b8b483ee68408
Jason Gignac <jasonpgignac@gmail.com>
2009-10-05 11:02:23 -0500

    Move to 1.9 CSV

--- app/models/package_test.rb
+++ app/models/package_test.rb
@@ -1,20 +1,15 @@
-require 'csv'
 class PackageTest < ActiveRecord::Base
   belongs_to    :package
   has_many      :package_test_reactions
   acts_as_list  :scope => :package
   
   def self.csv_headers
-    csv_string= CSV.generate do |csv|
-      csv << ["rule_type", "rule_parameter", "success_value"]
-    end
+    csv_string << ["rule_type", "rule_parameter", "success_value"].to_csv
     return csv_string
   end
   def to_csv
-    csv_string= CSV.generate do |csv|
-      sval = success_value ? 1 : 0
-      csv << ["TEST", rule_type, rule_parameter, sval]
-    end
+    sval = success_value ? 1 : 0
+    csv_string << ["TEST", rule_type, rule_parameter, sval].to_csv
     return csv_string.sub!("\n", "")
   end
 end 
--- app/models/package_test_reaction.rb
+++ app/models/package_test_reaction.rb
@@ -1,18 +1,13 @@
-require 'csv'
 class PackageTestReaction < ActiveRecord::Base
   belongs_to    :package_test
   acts_as_list  :scope => :package_test
   
   def self.csv_headers
-    csv_string= CSV.generate do |csv|
-      csv << ["command", "parameters", "repetitions"]
-    end
+    csv_string << ["command", "parameters", "repetitions"].to_csv
     return csv_string
   end
   def to_csv
-    csv_string= CSV.generate do |csv|
-      csv << ["ACTION", command, parameter, repetitions]
-    end
+    csv_string << ["ACTION", command, parameter, repetitions].to_csv
     return csv_string.sub!("\n", "")
   end
 end
